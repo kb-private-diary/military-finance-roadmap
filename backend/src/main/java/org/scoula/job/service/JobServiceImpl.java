f238a77ef4c74f22948a67113c6ec494ece9c5db
jiwon415 <parkjiwon3632@gmail.com>
2026-07-24 15:14:06 +0900

    ❇️[feat] 목표 등록 시 준비항목 복수 저장 기능 추가
    - job_interested_type 테이블 신설에 따른 목표 등록 로직 보완
    - JobInterestedTypeVO 신규, JobGoalCreateRequestDTO에 itemTypes 필드 추가
    - JobMapper/JobMapper.xml에 createJobInterestedType insert 추가
    - JobServiceImpl에서 목표 등록 시 선택된 준비 항목 각 한 줄씩 저장

--- backend/src/main/java/org/scoula/job/service/JobServiceImpl.java
+++ backend/src/main/java/org/scoula/job/service/JobServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.scoula.job.domain.JobCodeVO;
 import org.scoula.job.domain.JobGoalVO;
+import org.scoula.job.domain.JobInterestedTypeVO;
 import org.scoula.job.dto.JobCodeDTO;
 import org.scoula.job.dto.JobGoalCreateRequestDTO;
 import org.scoula.job.dto.JobGoalCreateResponseDTO;
@@ -38,6 +39,20 @@ public class JobServiceImpl implements JobService{
         jobGoalVO.setExpectedDate(requestDTO.getExpectedDate());
 
         jobMapper.createJobGoal(jobGoalVO);
+
+        // 선택된 준비 항목(P01/P02/P03)을 goal_id 기준으로 각 한 줄씩 저장
+        // job_goal insert와 같은 @Transactional 범위 안이라, 중간에 실패하면 전체 롤백됨
+        List<String> itemTypes = requestDTO.getItemTypes();
+        if (itemTypes != null) {
+            for (String itemType : itemTypes) {
+                JobInterestedTypeVO jobInterestedTypeVO = new JobInterestedTypeVO();
+                jobInterestedTypeVO.setGoalId(jobGoalVO.getGoalId());
+                jobInterestedTypeVO.setItemType(itemType);
+                // TODO: JWT 미연동으로 인해 job_goal insert와 동일하게 임시로 userId를 문자열로 기록
+                jobInterestedTypeVO.setCreatedNm(String.valueOf(requestDTO.getUserId()));
+                jobMapper.createJobInterestedType(jobInterestedTypeVO);
+            }
+        }
 
         return new JobGoalCreateResponseDTO(jobGoalVO.getGoalId());
     }
